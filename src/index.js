50ca6e9e5fa5b561cc1ba06bdb61fc7e4315bff5
Roza. <71328419+shinyPy@users.noreply.github.com>
2024-08-23 14:11:48 +0800

    more

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { LanguageProvider } from "./utils/LanguageContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <LanguageProvider>
     <React.StrictMode>
       <App />
+      <Analytics />
     </React.StrictMode>
   </LanguageProvider>,
 );
